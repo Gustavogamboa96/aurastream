db69e926e5263ed1d76717b597ebe2cdcf5b8176
Gustavogamboa96 <Gustavo.gamboa96@gmail.com>
2025-07-29 17:57:40 -0400

    10 suggestions

--- api/search.ts
+++ api/search.ts
@@ -40,7 +40,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     if (suggestions) {
       // Return top 3 torrents for suggestion cards
-      return res.json({ results: finalTorrents.slice(0, 3) });
+      return res.json({ results: finalTorrents.slice(0, 10) });
     }
     console.log('Search results:', finalTorrents);
     return res.json({ results: finalTorrents });
